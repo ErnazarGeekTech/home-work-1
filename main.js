94f7a2274e8d5d2371a6e696765582c3d4a32a3a
ErnazarGeekTech <2017smaiferloke@gmail.com>
2023-02-24 19:13:21 +0300

    add c

--- main.js
+++ main.js
@@ -1,44 +1,47 @@
 //1
-// function printArrayValues(arr, i = 0) {
-//     if (i < arr.length) {
-//       console.log(arr[i], 'array');
-//       printArrayValues(arr, i + 1);
-//     }
-//   }
+function printArrayValues(arr, i = 0) {
+    if (i < arr.length) {
+      console.log(arr[i], 'array');
+      printArrayValues(arr, i + 1);
+    }
+  }
   
-//   printArrayValues([1, 2, 3]);
+  printArrayValues([1, 2, 3]);
 
 //2
-// let arr = [4, 5, 6];
-// let newArr = [1, 2, 3].concat(arr);
+let arr = [4, 5, 6];
+let newArr = [1, 2, 3].concat(arr);
 
-// for (let i = 7; i <= 10; i++) {
-//   newArr.push(i);
-// }
+for (let i = 7; i <= 10; i++) {
+  newArr.push(i);
+}
 
-// console.log(newArr);
-// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(newArr);
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 //3
-// let arr1 = [1, 2, 3, 4, 5];
-// let lastElement = arr1[arr1.length - 1];
+let arr1 = [1, 2, 3, 4, 5];
+let lastElement = arr1[arr1.length - 1];
+console.log(lastElement);
+console.log(arr1[4]);
 
-// console.log(lastElement);
 
 //4
-// function countChar(str, char) {
-//     let count = 0;
-//     for (let i = 0; i < str.length; i++) {
-//       if (str[i] === char) {
-//         count++;
-//       }
-//     }
-//     return count;
-//   }
+function countChar(str, char) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === char) {
+        count++;
+      }
+    }
+    return count;
+  }
   
-//   let str = 'loremipsumdolor';
-//   let char = 'o';
-//   let charCount = countChar(str, char);
+  let str = 'loremipsumdolor';
+  let char = 'o';
+  let charCount = countChar(str, char);
   
-//   console.log(charCount);
-//   3
+  console.log(charCount);
+
+  console.log('Chyngyz');
+  
